0b7083001545d6803c010308eb2236da15be6f19
gaurav <gauravrakheja002@gmail.com>
2018-02-21 01:13:06 +0530

    made default role student in sign up, and hiding student fiels if selected role is teacher

---
+++ app/assets/javascripts/sign_up.js
@@ -0,0 +1,22 @@
+Turbolinks.setProgressBarDelay(1);
+
+$(document).ready(function(){
+	hideStudentFields();
+	$('#new_user').on("change", function(){
+		hideStudentFields();
+	})
+})
+
+
+var hideStudentFields = function(){
+	if ($('#user_role').val() == "student"){
+		$('#user_roll_number').parent().slideDown();
+		$('#user_course').parent().slideDown();
+		$('#user_semester').parent().slideDown();
+	}
+	else{
+		$('#user_roll_number').parent().slideUp();
+		$('#user_course').parent().slideUp();
+		$('#user_semester').parent().slideUp();	
+	}
+}
