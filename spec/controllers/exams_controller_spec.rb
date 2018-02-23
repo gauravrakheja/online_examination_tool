e3ed80f3df114961760ae37a527161c2e5c9e4d1
Gaurav <gauravrakheja002@gmail.com>
2018-02-23 21:49:22 +0530

    added specs for exams controller

---
+++ spec/controllers/exams_controller_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+describe ExamsController, type: :controller do
+  let(:teacher) { create(:teacher) }
+  
+  before do
+    sign_in teacher
+  end
+
+  describe '#create' do
+    let(:exam) { build(:exam) }
+
+    it 'should create the exam' do
+      expect(exam.persisted?).to eq false
+      post :create, params: { exam: exam.attributes }
+      expect(Exam.last.subject).to eq exam.subject
+      expect(Exam.last.title).to eq exam.title
+    end
+  end
+
+  describe '#index' do
+    let(:exam) { create(:exam) }
+    let(:exam1) { create(:exam) }
+    let(:q) { double(:q, result: [exam, exam1]) }
+
+    before do
+      allow(Exam).to receive(:ransack).and_return(q)
+    end
+
+    it 'should give all the exams' do
+      get :index
+      expect(assigns(:exams)).to include(exam, exam1)
+    end
+  end
+end
