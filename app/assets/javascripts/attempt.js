d26fa949d8d4f097e7d95f0ec42cc13ca5b0b791
Gaurav <gauravrakheja002@gmail.com>
2018-03-17 13:49:23 +0530

    cleaned up attempt.js

--- app/assets/javascripts/attempt.js
+++ app/assets/javascripts/attempt.js
@@ -1,15 +1,11 @@
 $(document).on("turbolinks:load",function(){
 	if(/attemps\/new/.test(window.location.href)){
-		var duration = $('#duration').attr('value');
-		duration = duration * 60000
-		duration = parseInt(duration);
-	  setTimeout(function() { submitAttempt(); },duration);
 		var x = setInterval(function() { showTimer(); }, 100);
 	};
 });
 
 function showTimer(){
-  // Get todays date and time
+  // Get date and time
   var date = $('#time').attr('value')
   var countDownDate = new Date(date).getTime();
   var now = new Date().getTime();
@@ -20,13 +16,13 @@ function showTimer(){
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  // Display the result in the element with id="demo"
   document.getElementById("demo").innerHTML = days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
-  // If the count down is finished, write some text 
+  //do something when time is up
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
+    document.getElementById("demo").innerHTML = "Time is Over";
+    submitAttempt();
   };
 };
 
